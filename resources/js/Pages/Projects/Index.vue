5c86cd2dc102fee8069b056706799f2c5d842587
mls42 <laspina92@gmail.com>
2024-12-29 11:26:17 +0100

    Add Header styles

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -11,7 +11,7 @@ defineProps(['projects']);
 
     <AppLayout title="Projects">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2>
                 Projects
             </h2>
         </template>
